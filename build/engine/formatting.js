c4a0fba5bf055e7d27f668ce57f4982aab6961b9
masashi katsumata <wf9a5m75@gmail.com>
2023-06-30 13:15:38 -0700

    Build ディレクトリの削除

--- build/engine/formatting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatResidentialSection = void 0;
-function formatResidentialSection(section) {
-    return [section.blk, section.addr1, section.addr2].filter(x => !!x).join('-');
-}
-exports.formatResidentialSection = formatResidentialSection;
