21d466b834389225dfcb1e380d5339b39b16ebf2
maskatsum <maskatsum@digital.go.jp>
2023-08-09 04:11:54 -0700

    Unit test for getConfigDir()

---
+++ src/cli/domain/__tests__/getConfigDir.spec.ts
@@ -0,0 +1,16 @@
+import { getDataDir } from '../getDataDir';
+import os from 'node:os';
+import path from 'node:path';
+import fs from 'node:fs';
+
+describe('getDataDir', () => {
+  it('should create a directory if not existed', async () => {
+    const workDir = os.tmpdir();
+    const dataDir = await getDataDir(path.join(workDir, 'something'));
+    expect(fs.existsSync(dataDir)).toBe(true);
+  });
+  it('should create a directory at the (os:home) directory', async () => {
+    const dataDir = await getDataDir();
+    expect(fs.existsSync(dataDir)).toBe(true);
+  });
+});
