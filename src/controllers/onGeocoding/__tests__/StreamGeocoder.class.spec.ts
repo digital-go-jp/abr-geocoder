9cbdb8d32a6b014e93737c2c5f57436b904685ea
maskatsum <maskatsum@digital.go.jp>
2023-09-15 00:07:36 -0700

    clean up unnecessary imports

--- src/controllers/onGeocoding/__tests__/StreamGeocoder.class.spec.ts
+++ src/controllers/onGeocoding/__tests__/StreamGeocoder.class.spec.ts
@@ -1,4 +1,4 @@
-import { jest, describe, expect, it, beforeEach } from '@jest/globals';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 jest.mock('../../../domain/jisKanji');
 // jest.mock('../../../domain/RegExpEx');
 // jest.mock('../../../domain');
@@ -24,30 +24,17 @@ import { JisKanji } from '../../../domain/jisKanji';
 const mockedJisKanji = JisKanji as jest.Mocked<typeof JisKanji>;
 mockedJisKanji.replaceAll.mockReturnValue('');
 
-import { StreamGeocoder } from '../StreamGeocoder.class';
 import { default as BetterSqlite3, default as Database } from 'better-sqlite3';
+import { Readable } from 'node:stream';
 import { PrefectureName } from '../../../domain/types/';
-import { GeocodingStep1 as Step1 } from '../step1-transform';
-import { GeocodingStep2 as Step2 } from '../step2-transform';
-import { GeocodingStep3 as Step3 } from '../step3-transform';
-import { GeocodingStep3A as Step3A } from '../step3a-transform';
-import { GeocodingStep3B as Step3B } from '../step3b-transform';
-import { GeocodingStep3Final as Step3Final } from '../step3final-transform';
-import { GeocodingStep4 as Step4 } from '../step4-transform';
-import { GeocodingStep5 as Step5 } from '../step5-transform';
-import { GeocodingStep6 as Step6 } from '../step6-transform';
-import { GeocodingStep7 as Step7 } from '../step7-transform';
-import { GeocodingStep8 as Step8 } from '../step8-transform';
-import { Readable, Writable } from 'node:stream';
 import * as PATCHES from '../../../settings/patchPatterns';
 import {
-  AddressFinderForStep3and5 as Addr3and5,
-  AddressFinderForStep7 as Addr7,
   getCityPatternsForEachPrefecture as getCityP,
   getPrefectureRegexPatterns as getPreRegP,
   getPrefecturesFromDB as getPrefs,
-  getSameNamedPrefecturePatterns as getSamePrefs,
+  getSameNamedPrefecturePatterns as getSamePrefs
 } from '../../../usecase';
+import { StreamGeocoder } from '../StreamGeocoder.class';
 
 // getPrefecturesFromDB
 const mockedGetPrefs = getPrefs as jest.Mocked<typeof getPrefs>;
