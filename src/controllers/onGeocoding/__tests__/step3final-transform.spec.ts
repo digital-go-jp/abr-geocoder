4af04f6691b447d94676b1660652ce21d4543ac9
maskatsum <maskatsum@digital.go.jp>
2023-10-09 21:54:43 -0700

    Create unit test for GeocodingStep3Final

---
+++ src/controllers/onGeocoding/__tests__/step3final-transform.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import Stream from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { FromStep3Type, Query } from '../../../domain';
+import { GeocodingStep3Final } from '../step3final-transform';
+
+describe('step3final-transform', () => {
+  it.concurrent('should involve the callback of the chunk', async () => {
+    
+    const fromStep3: FromStep3Type = {
+      query: Query.create('somewhere'),
+      callback: jest.fn(),
+    };
+    await pipeline(
+      Stream.Readable.from(
+        [
+          fromStep3,
+        ],
+        {
+          objectMode: true,
+        }
+      ),
+      new GeocodingStep3Final(),
+    );
+
+    expect(fromStep3.callback).toBeCalledWith(null, fromStep3.query);
+  });
+});
