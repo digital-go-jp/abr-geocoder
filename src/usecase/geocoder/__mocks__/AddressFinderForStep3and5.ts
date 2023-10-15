c1cd5ecfca901f3f63ab9f0e6da68153d486ee0d
maskatsum <maskatsum@digital.go.jp>
2023-10-14 21:09:23 -0700

    Write test for step5-transform.spec.ts

--- src/usecase/geocoder/__mocks__/AddressFinderForStep3and5.ts
+++ src/usecase/geocoder/__mocks__/AddressFinderForStep3and5.ts
@@ -6,6 +6,9 @@ export const AddressFinderForStep3and5 = jest.fn().mockImplementation(() => {
     find: (params: FindParameters) => {
       switch (params.prefecture) {
         case PrefectureName.TOKYO:
+          //
+          // Dummy result for step3b-transform.spec.ts
+          //
           return Promise.resolve({
             lg_code: '132063',
             town_id: '0001002',
@@ -16,8 +19,27 @@ export const AddressFinderForStep3and5 = jest.fn().mockImplementation(() => {
             originalName: '',
             tempAddress: '22番地の22',
           });
+
         case PrefectureName.HIROSHIMA:
+          //
+          // Dummy result for step3b-transform.spec.ts
+          //
           return Promise.resolve(null);
+
+        case PrefectureName.KYOTO:
+          //
+          // Dummy result for step5-transform.spec.ts
+          //
+          return Promise.resolve({
+            koaza: '',
+            lat: 34.877027, 
+            lg_code: '262102',
+            lon: 135.708529,
+            name: '八幡園内',
+            originalName: '',
+            tempAddress: '75', 
+            town_id: '0302000'
+          });
 
         default:
           throw new Error(
