d9c23836af2f78f7b8200bb8772ecb1fb99a2383
maskatsum <maskatsum@digital.go.jp>
2023-10-17 12:22:45 -0700

    Insert copyright header to all .ts files

--- src/interface-adapter/setup-container.ts
+++ src/interface-adapter/setup-container.ts
@@ -1,4 +1,29 @@
+/*!
+ * MIT License
+ *
+ * Copyright (c) 2023 デジタル庁
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 // reflect-metadata is necessary for DI
+import 'reflect-metadata';
+
 import { AbrgError, AbrgErrorLevel } from '@abrg-error/abrg-error';
 import { AbrgMessage } from '@abrg-message/abrg-message';
 import { DI_TOKEN } from '@interface-adapter/tokens';
@@ -6,7 +31,6 @@ import CLIInfinityProgress from 'cli-infinity-progress';
 import { MultiBar, SingleBar } from 'cli-progress';
 import fs from 'node:fs';
 import path from 'node:path';
-import 'reflect-metadata';
 import { DependencyContainer, container } from 'tsyringe';
 import { CsvTransform } from './formatters/csv-transform';
 import { GeoJsonTransform } from './formatters/geo-json-transform';
