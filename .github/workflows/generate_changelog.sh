6bc217f89dd02b9599180fed3cc66caa09d4245a
Masashi Katsumata <maskatsum@digital.go.jp>
2023-10-18 20:32:54 -0700

    test: release npm automatically

---
+++ .github/workflows/generate_changelog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version=$(cat package.json | jq -r '.version')
+
+last_commit_hash=$(git show-ref --tags | head -n1 | cut -f 1 -d " " | tr -d '\n')
+last_commit_iso8601=$(git show -s --format="%cI" $last_commit_hash |tail -n 1 | tr -d '\n')
+name_with_owner=$(gh repo view --json nameWithOwner -q ".nameWithOwner" | tr -d '\n')
+pr_list=$(gh pr list --search "merged:>${last_commit_iso8601}" --repo ${name_with_owner})
+
+echo "# release notes" > release_notes/${version}.md
+echo "- version: ${version}" >> release_notes/${version}.md
+IFS=$'\n'
+for line in $pr_list; do
+  ticket=$(echo ${line} | cut -f 1)
+  title=$(echo ${line} | cut -f 2)
+  echo "[#${ticket}](https://github.com/${name_with_owner}/pull/${ticket})  ${title}" >> release_notes/${version}.md
+done
