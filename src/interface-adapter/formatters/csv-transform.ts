0a75273399464d6f5d914346813a9645e43418ab
Masashi Katsumata <maskatsum@digital.go.jp>
2024-02-15 22:42:09 -0800

    wrap the input and the other fields with double quotations

--- src/interface-adapter/formatters/csv-transform.ts
+++ src/interface-adapter/formatters/csv-transform.ts
@@ -58,7 +58,7 @@ export class CsvTransform extends Stream.Transform {
       .map(column => {
         switch (column) {
           case GeocodeResultFields.INPUT:
-            return result.input;
+            return `"${result.input || ''}"`;
 
           case GeocodeResultFields.OUTPUT:
             return `"${result.output || ''}"`;
@@ -88,7 +88,7 @@ export class CsvTransform extends Stream.Transform {
             return result.town_id || '';
 
           case GeocodeResultFields.OTHER:
-            return result.other || '';
+            return `"${result.other || ''}"`;
 
           case GeocodeResultFields.BLOCK:
             return result.block || '';
