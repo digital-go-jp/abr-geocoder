ced72de5abbedd27f3254517ac3b4d9ead9c340c
yaaasuu <anyongw01123@gmail.com>
2024-08-04 16:37:57 +0900

    test

---
+++ src/domain/services/package/__tetst__/parse-package-json.test.ts
@@ -0,0 +1,57 @@
+import { jest, test, describe, expect, beforeAll } from "@jest/globals";
+import fs from 'node:fs';
+import { parsePackageJson } from "../parse-package-json";
+
+jest.mock('node:fs');
+
+const mockFsReadFileSync = fs.readFileSync as jest.MockedFunction<typeof fs.readFileSync>;
+
+// mockFsReadFileSync.mockImplementation((path) => {
+//     return `
+//     {
+//     "version": "dummy",
+//     "description": "dummy description"
+//     }` as string | Buffer;
+// })
+mockFsReadFileSync.mockReturnValue(`
+{
+    "version": "dummy",
+    "description": "dummy description"
+}
+`)
+
+describe("persePackageJson", () => {
+    beforeAll(() => {
+        mockFsReadFileSync.mockReset();
+    });
+    test("test1", () => {
+        mockFsReadFileSync.mockReturnValue(`
+            {
+            "version": "dummy",
+            "description": "dummy description"
+            }
+        `);
+        const result = parsePackageJson({
+            filePath: 'dummy.json',
+        });
+        expect(result).toEqual({
+            version: "dummy",
+            description: "dummy description"
+        });
+    });
+    
+    test("test2", () => {
+        mockFsReadFileSync.mockReset();
+        mockFsReadFileSync.mockImplementation(() =>{
+            throw'File not found'
+        })
+
+    expect(() => {
+        parsePackageJson({
+            filePath: 'error.json',
+        });
+    }).toThrow('File not found');
+    });
+
+
+})
