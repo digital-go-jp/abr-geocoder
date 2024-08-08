cc9cbd198f9b2ed5b72105b51ae0acf2b629881d
sakabe005 <sakabe1201@gmail.com>
2024-08-08 23:36:12 +0900

    small arrange

--- src/usecases/geocode/services/__test__/kan2num.test.ts
+++ src/usecases/geocode/services/__test__/kan2num.test.ts
@@ -9,9 +9,14 @@ describe('kan2num', () =>{
         expect(result).toBe('ソルティア岡本1');
     });
     
-    test('test3', () => {
+    test('test2', () => {
         const result = kan2num('ソルティア岡本四十四');
         expect(result).toBe('ソルティア岡本44');
+    });
+
+    test('test3', () => {
+        const result = kan2num('二十五丁目三十一番地');
+        expect(result).toBe('25丁目31番地');
     });
     
 });
