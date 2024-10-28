888d4d2a152e9d08f5c4bbed7a47d3ef856f6bd9
Masashi Katsumata <wf9a5m75@gmail.com>
2024-10-28 02:56:51 -0700

    Fix: build error

--- src/usecases/geocode/models/__tests__/oaza-cho-trie-finder.test.ts
+++ src/usecases/geocode/models/__tests__/oaza-cho-trie-finder.test.ts
@@ -5,11 +5,11 @@ import { OazaChoTrieFinder } from "../oaza-cho-trie-finder";
 describe('OazaChoTrieFinder', () => {
 
   test('should convert "軒", "条" and "丁目" to DASH symbols', async () => {
-    const result = OazaChoTrieFinder.normalizeStr('二十四軒三条一丁目');
+    const result = OazaChoTrieFinder.normalize('二十四軒三条一丁目');
     expect(result).toBe(`24${DASH}3${DASH}1`);
   });
   test('should convert "軒", "条", "丁目" and "の" to DASH symbols', async () => {
-    const result = OazaChoTrieFinder.normalizeStr('二十四軒三条一丁目三の三');
+    const result = OazaChoTrieFinder.normalize('二十四軒三条一丁目三の三');
     expect(result).toBe(`24${DASH}3${DASH}1${DASH}3${DASH}3`);
   });
 });
